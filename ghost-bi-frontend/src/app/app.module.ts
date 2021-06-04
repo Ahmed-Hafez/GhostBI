98bdc4630432bf0d31e857b12150cb98c303bef6
Ahmed Hafez <41956720+Ahmed-Hafez@users.noreply.github.com>
2021-06-04 14:46:48 +0200

    Adding Initial project files
    Adding initial project files with the required libraries and dependcies.
    Also adding navbar and side-bar components

---
+++ ghost-bi-frontend/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SideBarComponent } from './components/side-bar/side-bar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    SideBarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
