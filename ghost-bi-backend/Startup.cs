beca8e758e00ebf24777172a9fd65434ba8bc4af
Ahmed Hafez <41956720+Ahmed-Hafez@users.noreply.github.com>
2021-06-05 06:50:00 +0200

    Adding Dependencies, Configuring PostgresSQL database

--- ghost-bi-backend/Startup.cs
+++ ghost-bi-backend/Startup.cs
@@ -2,10 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ghost_bi_backend.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -16,6 +18,7 @@ namespace ghost_bi_backend
 {
     public class Startup
     {
+        private string connectionString;
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -26,12 +29,15 @@ namespace ghost_bi_backend
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            this.connectionString = Configuration["secretConnectionString"];
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "ghost_bi_backend", Version = "v1" });
             });
+
+            services.AddEntityFrameworkNpgsql()
+                    .AddDbContext<BiDbContext>(opt => opt.UseNpgsql(connectionString));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
